a95c0da54a31d47d4d0599f0b7818f9fe23aa8bf
Netkas <netkas@intellivoid.info>
2021-02-04 12:17:17 -0500

    Added Database constructor

--- src/DeepAnalyticsManager/DeepAnalyticsManager.php
+++ src/DeepAnalyticsManager/DeepAnalyticsManager.php
@@ -1,10 +1,14 @@
 <?php
 
+    /** @noinspection PhpUnused */
+    /** @noinspection PhpMissingFieldTypeInspection */
+
 
     namespace DeepAnalyticsManager;
 
     use acm\acm;
     use Exception;
+    use mysqli;
 
     require_once(__DIR__ . DIRECTORY_SEPARATOR . 'AutoConfig.php');
 
@@ -24,6 +28,11 @@
         private $DatabaseConfiguration;
 
         /**
+         * @var null
+         */
+        private $database;
+
+        /**
          * CoffeeHouse constructor.
          * @throws Exception
          */
@@ -31,5 +40,49 @@
         {
             $this->acm = new acm(__DIR__, 'Deep Analytics Manager');
             $this->DatabaseConfiguration = $this->acm->getConfiguration('Database');
+
+            $this->database = null;
+        }
+
+        /**
+         * @return mysqli|null
+         */
+        public function getDatabase(): ?mysqli
+        {
+            if($this->database == null)
+            {
+                $this->connectDatabase();
+            }
+
+            return $this->database;
+        }
+
+
+        /**
+         * Closes the current database connection
+         */
+        public function disconnectDatabase()
+        {
+            $this->database->close();
+            $this->database = null;
+        }
+
+        /**
+         * Creates a new database connection
+         */
+        public function connectDatabase()
+        {
+            if($this->database !== null)
+            {
+                $this->disconnectDatabase();
+            }
+
+            $this->database = new mysqli(
+                $this->DatabaseConfiguration['Host'],
+                $this->DatabaseConfiguration['Username'],
+                $this->DatabaseConfiguration['Password'],
+                $this->DatabaseConfiguration['Name'],
+                $this->DatabaseConfiguration['Port']
+            );
         }
     }
